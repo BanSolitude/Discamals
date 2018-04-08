99713c2f3eef9eb7ba9bd7dbc186225079247c83
Daniel Busto <danielsbuto@gmail.com>
2018-04-07 21:58:56 -0400

    First iteration of more complex tournaments. For now just a 4-man single elim tournament (larger single elim tournaments would just require generating more players).

--- DiscamalsGame.py
+++ DiscamalsGame.py
@@ -14,11 +14,9 @@ def add_disc(FreeAnimalNames, UsedAnimalNames):
 	UsedAnimalNames.append(new_animal)
 	return new_animal
 
-#Teams should be lists.
+#Teams are frozensets.
 def get_winner(Team1, Team2, WinnerList):
-	Team1 = frozenset(Team1)
-	Team2 = frozenset(Team2)
-
+	#TODO is a case for a "bye" needed here?
 	if len(Team1) != len(Team2) or Team1 == Team2:
 		return None
 
@@ -48,12 +46,11 @@ def test(PlayersDiscamals, WinnerList):
 	print ("Choose two Discamals to discover the winner.")
 
 	print ("First Discamal")
-	_d1 = select_disc_manually(PlayersDiscamals)
+	_d1 = select_team_manually(PlayersDiscamals)
 	print ("Second Discamal")
-	_d2 = select_disc_manually([d for d in PlayersDiscamals if d!=_d1])
+	_d2 = select_team_manually([d for d in PlayersDiscamals if d!=_d1])
 
-	for _disc in get_winner([_d1], [_d2], WinnerList):
-		print(_disc)
+	print("%s wins the test." % format_team(get_winner(_d1, _d2, WinnerList)))
 
 	print("")
 
@@ -63,30 +60,49 @@ def test(PlayersDiscamals, WinnerList):
 def tournament(AvailableDiscamals, Players, WinnerList):
 	print("Choose a Discamal to take to the tournament.")
 	for player in Players:
-		player.select_disc(AvailableDiscamals)
+		player.select_team(AvailableDiscamals)
 
-	#this is just a work around until tounament is improved.
-	_dplayer= Players[0].currentDisc
-	_dcomp = Players[1].currentDisc
-	print ("Your opponent chose", _dcomp)
-	if (_dplayer == _dcomp):
-		_player_wins = choice([True, False])
-		if (_player_wins):
-			print("Lucky you won the mirror match!")
-		else:
-			print("Unfortunately you lost the mirror match.")
-
-	elif (get_winner([_dplayer], [_dcomp], WinnerList) == frozenset([_dplayer])):
-		print("And you win!")
-	else:
-		print ("You lose.")
+	#this is just a work around until tounament code is improved.
+	#for now single elim. plan is swiss to top x (8?)
+	single_elim(Players, WinnerList)
 
 	print("")
+
+def single_elim(Players, WinnerList):
+	_cur_round = [player for player in Players]
+	while len(_cur_round) > 1:
+		_temp = _cur_round
+		_next_round = []
+		while len(_temp) > 0:
+			_p1 = _temp.pop()
+			_p2 = _temp.pop()
+			_do_print = _p1.selectTeam == select_team_manually or _p2.selectTeam == select_team_manually
+			if _do_print:
+				print ("%s selected %s facing %s with %s." % (_p1.name, format_team(_p1.currentTeam), _p2.name, format_team(_p2.currentTeam)))
+			_winner = None
+			if (_p1.currentTeam == _p2.currentTeam):
+				_winner = choice([_p1, _p2])
+			else:
+				_winning_team = get_winner(_p1.currentTeam, _p2.currentTeam, WinnerList)
+				if (_winning_team == _p1.currentTeam):
+					_winner = _p1
+				else:
+					_winner = _p2
+			if _do_print:
+				print ("%s wins!" % _winner.name)
+			_next_round.append(_winner)
+
+		_cur_round = _next_round
+
+	print ("%s won the tournament with %s." % (_cur_round[0].name, format_team(_cur_round[0].currentTeam)))
+
+def format_team(Team):
+	return ' '.join(Team)
 
 #TODO Some sort of economy
 #TODO Different number of tests/tournaments as the number of discamals goes up.
 if __name__ == '__main__':
-	_players = [Player(input("Please enter your name:"), select_disc_manually), Player("Randy", select_disc_randomly)]
+	_players = [Player(input("Please enter your name:"), select_team_manually), Player("Randy", select_team_randomly), Player("Andy", select_team_randomly), Player("Mandy", select_team_randomly)]
 
 	print ("Here are the starting Discamals:")
 	add_disc(FREE_ANIMAL_NAMES, USED_ANIMAL_NAMES)
--- Player.py
+++ Player.py
@@ -3,23 +3,24 @@ from random import choice
 class Player:
 	def __init__(self, Name, Strategy):
 		self.name = Name
-		self.selectDisc = Strategy
-		self.currentDisc = None
+		self.selectTeam = Strategy
+		self.currentTeam = None
 
 	def __unicode__(self):
 		return self.name
 
-	def select_disc(self, AvailableDiscamals):
-		self.currentDisc = self.selectDisc(AvailableDiscamals)
+	def select_team(self, AvailableDiscamals):
+		self.currentTeam = self.selectTeam(AvailableDiscamals)
 
-def select_disc_randomly(AvailableDiscamals):
-	return choice(AvailableDiscamals)
+#TODO make these work for teams of arbitrary length
+def select_team_randomly(AvailableDiscamals):
+	return frozenset([choice(AvailableDiscamals)])
 
-def select_disc_manually(PlayersDiscamals):
+def select_team_manually(PlayersDiscamals):
 	_dplayer = ""
 	while (not _dplayer in PlayersDiscamals):
 		_dplayer = input("Please enter your choice of discamal:").lower()
 		if (not _dplayer in PlayersDiscamals):
 			print ("Your selection was invalid, please try again")
 
-	return _dplayer
+	return frozenset([_dplayer])
