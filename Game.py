9972415fd03f85a7273f87752b1e7782043b009f
Daniel Busto <danielsbuto@gmail.com>
2018-04-14 12:07:12 -0400

    Changed some prints to inputs in order to slow down the flow of text and make it more readible

--- Game.py
+++ Game.py
@@ -10,7 +10,7 @@ WINNER = {}
 
 def add_disc(DiscFactory):
 	_new_disc = DiscFactory.new_discamal()
-	print("New disc is %s" % _new_disc.name)
+	input("New disc is %s" % _new_disc.name)
 	DISC_LIST.append(_new_disc)
 	return _new_disc
 
@@ -23,7 +23,6 @@ def get_winner(Team1, Team2, WinnerList):
 	if  ( not (frozenset([Team1,Team2]) in WinnerList) ):
 		if (len(Team1) == 1):
 			WinnerList[frozenset([Team1,Team2])] = choice([Team1,Team2])
-
 		else:
 			_score1 = 0
 			_score2 = 0
@@ -42,7 +41,7 @@ def get_winner(Team1, Team2, WinnerList):
 #TODO parse input in a better way
 #TODO print messages out in a way that's easier to read
 def test(PlayersDiscamals, WinnerList):
-	print ("Choose two teams to discover the winner.")
+	print("Choose two teams to discover the winner.")
 
 	_team_size = None
 	while not (type(_team_size) == int):
@@ -50,14 +49,14 @@ def test(PlayersDiscamals, WinnerList):
 		try:
 			_team_size = int(_team_size)
 		except (Exception):
-			print ("Please enter a valid integer.")
+			input("Please enter a valid integer.")
 
-	print ("First Team")
+	print("First Team")
 	_d1 = select_team_manually(PlayersDiscamals, _team_size)
-	print ("Second Team")
+	print("Second Team")
 	_d2 = select_team_manually(PlayersDiscamals, _team_size)
 
-	print("%s wins the test." % format_team(get_winner(_d1, _d2, WinnerList)))
+	input("%s wins the test." % format_team(get_winner(_d1, _d2, WinnerList)))
 
 	print("")
 
@@ -72,7 +71,7 @@ def tournament(AvailableDiscamals, Players, WinnerList, TeamSize):
 	_winner = single_elim(Players, WinnerList)
 	for _disc in _winner.currentTeam:
 		_disc.tournamentWins += 1
-	print ("%s won the tournament with %s." % (_winner.name, format_team(_winner.currentTeam)))
+	input("%s won the tournament with %s." % (_winner.name, format_team(_winner.currentTeam)))
 
 	print("")
 
@@ -86,7 +85,7 @@ def single_elim(Players, WinnerList):
 			_p2 = _temp.pop()
 			_do_print = _p1.selectTeam == select_team_manually or _p2.selectTeam == select_team_manually
 			if _do_print:
-				print ("%s selected %s facing %s with %s." % (_p1.name, format_team(_p1.currentTeam), _p2.name, format_team(_p2.currentTeam)))
+				print("%s selected %s facing %s with %s." % (_p1.name, format_team(_p1.currentTeam), _p2.name, format_team(_p2.currentTeam)))
 			_winner = None
 			if (_p1.currentTeam == _p2.currentTeam):
 				_winner = choice([_p1, _p2])
@@ -97,7 +96,7 @@ def single_elim(Players, WinnerList):
 				else:
 					_winner = _p2
 			if _do_print:
-				print ("%s wins!" % _winner.name)
+				input("%s wins!" % _winner.name)
 			_next_round.append(_winner)
 
 		_cur_round = _next_round
@@ -116,11 +115,11 @@ if __name__ == '__main__':
 			    Player("Mandy", select_team_most_wins)]
 	_disc_factory = DiscamalFactory(ANIMAL_NAMES)
 
-	print ("Here are the starting Discamals:")
+	print("Here are the starting Discamals:")
 	add_disc(_disc_factory)
 	add_disc(_disc_factory)
-	print ("Lucky Snow would like to offer you these two Discamals free if you agree to participate in our tournament tomorrow!")
-	print ("Take today to try them out.")
+	print("Lucky Snow would like to offer you these two Discamals free if you agree to participate in our tournament tomorrow!")
+	input("Take today to try them out.")
 
 	test(DISC_LIST, WINNER)
 	while True:
@@ -132,6 +131,6 @@ if __name__ == '__main__':
 		_team_size  = choice([1,2])
 		tournament(DISC_LIST, _players, WINNER, _team_size)
 		_new = add_disc(_disc_factory)
-		print ("The new discamal is %s. Hope you enjoy it!" % _new)
 
-	print ("End of game.")
+
+	input("End of game.")
