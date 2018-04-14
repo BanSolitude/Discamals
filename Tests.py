2cac4b7fd29a018f62935f9ba36713e97096aca0
Daniel Busto <danielsbuto@gmail.com>
2018-04-14 16:42:00 -0400

    First pass at some real test cases

--- Tests.py
+++ Tests.py
@@ -1,22 +1,66 @@
 import unittest
-import DiscamalsGame
+import Discamal
+import Game
 
-class TestDiscamals(unittest.TestCase):
-    _usedAnimalNames = []
-    _freeAnimalNames = ["testanimal","manytests"]
+class TestDiscamal(unittest.TestCase):
+    def test_NewDicamal_ReturnsNewDiscamal(self):
+        factory = Discamal.DiscamalFactory(["test"])
+        disc = factory.new_discamal()
+        self.assertEqual(disc.name, "test")
+        self.assertEqual(disc.tournamentWins, 0)
 
-    def test_AddDisc_AddsDiscToUsedAnimalNames(self):
-        length_before = len(self._usedAnimalNames)
-        DiscamalsGame.add_disc(self._freeAnimalNames, self._usedAnimalNames)
-        unittest.assertEqual(length_before + 1, len(self._usedAnimalNames))
+    def test_NewDiscamalRepeated_ReturnsDifferentDiscs(self):
+        factory = Discamal.DiscamalFactory(["test","other"])
+        disc = factory.new_discamal()
+        other = factory.new_discamal()
+        self.assertNotEqual(disc,other)
 
-    def test_AddDisc_RemovesDiscFromFreeAnimalNames(self):
-        length_before = len(self._freeAnimalNames)
-        DiscamalsGame.add_disc(self._freeAnimalNames, self._usedAnimalNames)
-        unittest.assertEqual(length_before - 1, len(self._freeAnimalNames))
+class TestGame(unittest.TestCase):
 
-    def test_GetWinner_WinnerExistsInDict(self):
-        pass
+    def test_GetWinner_ReturnsOneOfTheInputs(self):
+        WinnerList = {}
+        team1 = frozenset([Discamal.Discamal("test")])
+        team2 = frozenset([Discamal.Discamal("other")])
+        result = Game.get_winner(team1, team2, WinnerList)
+        self.assertIn(result, [team1,team2])
+
+    def test_GetWinner_IsConsistent(self):
+        WinnerList = {}
+        team1 = frozenset([Discamal.Discamal("test")])
+        team2 = frozenset([Discamal.Discamal("other")])
+        first = Game.get_winner(team1, team2, WinnerList)
+        second = Game.get_winner(team1, team2, WinnerList)
+        self.assertEqual(first, second)
+
+    def test_GetWinner_ChangeOrderIsConsistent(self):
+        WinnerList = {}
+        team1 = frozenset([Discamal.Discamal("test")])
+        team2 = frozenset([Discamal.Discamal("other")])
+        first = Game.get_winner(team1, team2, WinnerList)
+        second = Game.get_winner(team2, team1, WinnerList)
+        self.assertEqual(first, second)
+
+    def test_GetWinner_DominatedTeamLoses(self):
+        disc1 = Discamal.Discamal("one")
+        disc1t = frozenset([disc1])
+        disc2 = Discamal.Discamal("two")
+        disc2t = frozenset([disc2])
+        disc3 = Discamal.Discamal("three")
+        disc3t = frozenset([disc3])
+        disc4 = Discamal.Discamal("four")
+        disc4t = frozenset([disc4])
+
+        WinnerList = {frozenset([disc1t,disc3t]):disc1t,
+                      frozenset([disc1t,disc4t]):disc1t,
+                      frozenset([disc2t,disc3t]):disc2t,
+                      frozenset([disc2t,disc4t]):disc2t}
+        team1 = frozenset([disc1,disc2])
+        team2 = frozenset([disc3,disc4])
+        self.assertEqual(Game.get_winner(team1, team2, WinnerList), team1)
+
+    def test_FormatTeam_ReturnsNameList(self):
+        team = frozenset( [Discamal.Discamal("one"),  Discamal.Discamal("two")] )
+        self.assertIn(Game.format_team(team), ["one two", "two one"])
 
 if __name__ == '__main__':
     unittest.main()
